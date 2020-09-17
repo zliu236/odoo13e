2b86cd6cd51faaeed477bd26a16ed68861158173
zliu236 <zliu236@live.com>
2020-09-17 15:34:22 +0800

    Delete hello.py

--- hello.py
+++
@@ -1,7 +0,0 @@
-# TODO：#...this is a first app. @zliu236 test @ zliu236 ...... # this is 
-import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-
-x = range(1, 10, 10)
-print(x)
